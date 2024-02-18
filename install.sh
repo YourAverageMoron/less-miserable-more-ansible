062ccc36bcabc442d77d4411ea4b96e73b2e5491
Ryan <34078850+YourAverageMoron@users.noreply.github.com>
2024-02-18 12:02:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+sudo ansible-pull -U https://github.com/YourAverageMoron/less-miserable-more-ansible.git
