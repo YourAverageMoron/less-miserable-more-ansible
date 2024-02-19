790424fa44d15a96b56cfde6cded7a1882eeb1bf
Ryan <34078850+YourAverageMoron@users.noreply.github.com>
2024-02-19 11:34:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-sudo ansible-pull --vault-id less-miserable-more-ansible@less-miserable-more-ansible.txt --vault-id keep-your-secrets@keep-your-secrets.txt -U https://github.com/YourAverageMoron/less-miserable-more-ansible.git
+sudo ansible-pull --vault-id less-miserable-more-ansible@prompt --vault-id keep-your-secrets@prompt -U https://github.com/YourAverageMoron/less-miserable-more-ansible.git
