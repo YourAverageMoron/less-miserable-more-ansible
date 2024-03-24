dafef7662562026dc5781a0161d30fe81bef9ba2
Ryan <34078850+YourAverageMoron@users.noreply.github.com>
2024-03-24 12:05:25 +0000

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,3 +1,6 @@
 #/bin/zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ansible
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ryanjones/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
